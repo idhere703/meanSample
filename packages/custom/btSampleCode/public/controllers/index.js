3b47b55830823a9094544db1c2d30126babdb6aa
Brandon <brandon@gooddonegreat.com>
2016-05-24 19:11:43 -0400

    Basic structure.

--- packages/custom/btSampleCode/public/controllers/index.js
+++ packages/custom/btSampleCode/public/controllers/index.js
@@ -1,58 +1,32 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
-  function($scope, Global) {
-    $scope.global = Global;
-    $scope.sites = {
-      'makeapoint': {
-        'name': 'makeapoint',
-        'text': 'Makeapoint is a platform to craft and fine-tune ideas and messages providing a graphical experience which brough an offline methodlogy online',
-        'author': 'Linnovate',
-        'link': 'http://www.linnovate.net',
-        'image': '/meanStarter/assets/img/makeapoint.png'
-      },
-      'cactus': {
-        'name': 'Cactus Intranet',
-        'text': 'Cactus Intranet is an enterprise social network with features like real-time newsfeed, notifications, groups, events, polls, referral system etc. The system has role based permission system, allowing different stakeholders access and controls relevant to them.',
-        'author': 'QED42',
-        'link': 'http://www.qed42.com',
-        'image': '/meanStarter/assets/img/cactus.png'
-      }
-    };
-    $scope.packages = {
-      'gmap': {
-        'name': 'gmap',
-        'text': 'gmap lets you add geographical information to your applications objects',
-        'author': 'linnovate',
-        'link': 'http://www.qed42.com',
-        'image': '/meanStarter/assets/img/gmap.png'
-      },
-      'upload': {
-        'name': 'Upload',
-        'text': 'hello text',
-        'author': 'Linnovate',
-        'link': 'http://www.linnovate.net',
-        'image': 'http://cdn.designbyhumans.com/pictures/blog/09-2013/pop-culture-cats/Pop_Culture_Cats_Hamilton_Hipster.jpg'
-      },
-      'socket': {
-        'name': 'Socket',
-        'text': 'Socket.io support',
-        'author': 'Linnovate',
-        'link': 'http://www.linnovate.net',
-        'image': 'http://cdn.designbyhumans.com/pictures/blog/09-2013/pop-culture-cats/Pop_Culture_Cats_Hamilton_Hipster.jpg'
-      }
-    };
+angular.module('mean.system').controller('BtIndexController', ['$scope', function($scope) {
 
-    $scope.$watch(function() {
-      for (var i = 0; i < $scope.sites.length; i += 1) {
-        if ($scope.sites[i].active) {
-          return $scope.sites[i];
-        }
-      }
-    }, function(currentSlide, previousSlide) {
-      if (currentSlide !== previousSlide) {
-        console.log('currentSlide:', currentSlide);
+  // Vars
+  $scope.todos = [];
+  $scope.newItem = "";
+  $scope.tempId = 0;
+
+
+  $scope.addTodo = function() {
+    $scope.todos.push({title: $scope.newItem, complete: false, id: $scope.tempId});
+    $scope.tempId++;
+    $scope.newItem = "";
+  };
+
+  $scope.editTodo = function() {};
+
+  $scope.getTodo = function() {};
+
+  $scope.deleteTodo = function(item) {
+    $scope.todos = $scope.todos.filter(function(todo) {
+      // Only return the ones we want to keep.
+      if(todo.id !== item.id) {
+        return todo;
       }
     });
-  }
-]);
+  };
+
+
+
+}]);
--- packages/custom/btSampleCode/public/routes/system.js
+++ packages/custom/btSampleCode/public/routes/system.js
@@ -1,13 +1,10 @@
 'use strict';
 
-//Setting up route
-angular.module('mean.meanStarter').config(['$meanStateProvider', '$urlRouterProvider',
-  function($meanStateProvider, $urlRouterProvider) {
+angular.module('mean.meanStarter').config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
     // For unmatched routes:
     $urlRouterProvider.otherwise('/');
-
-    // states for my app
-    $meanStateProvider
+    $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'meanStarter/views/system/index.html'
